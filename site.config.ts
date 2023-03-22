5cdaaffaf57ab557797a87d6238a2aaeb05080b9
Duyduckgo <128560085+Duyduckgo@users.noreply.github.com>
2023-03-22 08:40:29 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,30 +1,35 @@
 import { siteConfig } from './lib/site-config'
 
+
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'ROA-eaaf90d4860146049a46aa4786d2987f',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
+
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
-  domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  name: 'riverofart.COM - Decorate Fountain',
+  domain: 'riverofart.com',
+  author: 'Duy Dang',
+
 
   // open graph metadata (optional)
-  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
+  description: 'Kiến tạo cảnh quan ngoại thất, chăm sóc nông nghiệp',
+
 
   // social usernames (optional)
-  twitter: 'khoa_nvk',
+  twitter: 'river_of_art',
   // github: 'transitive-bullshit',
-  linkedin: 'khoanguyen08',
-  
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  linkedin: 'Văn Duy Đặng',
+ 
+  facebook: 'https://www.facebook.com/profile.php?id=100090728059751',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'https://www.youtube.com/channel/UCnXF6_7onoKdYHabp7zDFog', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -32,13 +37,16 @@ export default siteConfig({
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
+
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
+
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
   isRedisEnabled: false,
+
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -51,21 +59,22 @@ export default siteConfig({
   },
   // pageUrlOverrides: null,
 
+
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   // navigationStyle: 'default',
   navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: 'Blog',
+      title: 'Đèn',
       pageId: '59432e1a0cdf4effbb402701deff00a9'
     },    
     {
-      title: 'NoCode VN',
+      title: 'Đầu phun nghệ thuật',
       pageId: '93331fa2964045248502bd314019c917'
     },    
     {
-      title: 'Khoá học',
+      title: 'Tưới cây',
       pageId: '8e761b9e7d1b4db1ae76bbc1e4f9aa1f'
     },    
     {
@@ -74,3 +83,5 @@ export default siteConfig({
     }
   ]
 })
+
+
